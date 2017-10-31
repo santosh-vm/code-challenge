c2f9e011c163171e13bffa51e236e62a1469aeeb
Santosh V. Madhyastha <santosh@gogii.net>
2017-10-30 20:03:05 -0700

    Adding the search Menu item in the action bar

--- app/src/main/java/android/santosh/com/luckycodechallenge/MainActivity.java
+++ app/src/main/java/android/santosh/com/luckycodechallenge/MainActivity.java
@@ -1,13 +1,46 @@
 package android.santosh.com.luckycodechallenge;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+    private static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main_activity, menu);
+
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(this);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        //TODO: implement filter logic.
+        Log.d(TAG, "onQueryTextSubmit, query: " + query);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        Log.d(TAG, "onQueryTextChange, newText: " + newText);
+        return false;
     }
 }
